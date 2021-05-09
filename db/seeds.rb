255d703a59c4c19506fc12354d07d5871976c324
yuma.wada <yuma.wada.1127@gmail.com>
2021-05-10 07:14:05 +0900

    Add: seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+begin
+  post = Post.count
+  if post < 10
+    (10 - post).times do |i|
+      Post.create!(
+        title: "title_#{i}",
+        body: "body_#{i}"
+      )
+    end
+  end
+rescue => e
+  puts "========== Error! =========="
+  puts e.message
+ensure
+  puts "********** Finished Seed. **********"
+  puts "Post: #{Post.count}records"
+end
